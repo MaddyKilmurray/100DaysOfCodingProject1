3446e8a3ced99874cffef81e320e89f6ac5f8724
Kilmurray <maddy.kilsbymcmurray@gmail.com>
2021-06-17 19:29:07 +0100

    third_commit

--- app/src/main/java/com/example/java_tutorial_150621/MainActivity.java
+++ app/src/main/java/com/example/java_tutorial_150621/MainActivity.java
@@ -18,6 +18,7 @@ import com.example.java_tutorial_150621.databinding.ActivityMainBinding;
 import android.view.Menu;
 import android.view.MenuItem;
 
+
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration appBarConfiguration;
